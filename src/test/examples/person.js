f86bd758a8319e0d0e595a7b00fdd7a266f65947
Jeroen Valk <jeroen@agentsatwork.nl>
2016-07-21 20:10:07 +0200

    updated example

--- src/test/examples/person.js
+++ src/test/examples/person.js
@@ -1,105 +1,52 @@
-module.exports = function(_) {
-	var isName = function person$isName(val) {
-		return /^\w+$/.test(val) && val.charAt(0).toUpperCase() === val.charAt(0) && val.substr(1).toLowerCase() === val.substr(1);
-	};
+module.exports = function (_) {
+    var isName = function person$isName(val) {
+        return /^\w+$/.test(val) && val.charAt(0).toUpperCase() === val.charAt(0) && val.substr(1).toLowerCase() === val.substr(1);
+    };
 
-	var findPersonByName = function person$findPersonByName(name) {
-		return _.findOne({
-			Person : {
-				'@' : {
-					where : {
-						name : name
-					}
-				},
-				Entity : {
-					'@' : {
-						where : {
-							type : 'person'
-						}
-					}
-				}
-			}
-		});
-	};
-
-	return {
-		match : {
-			_ : 'familyTree'
-		},
-		validation : {
-			'_META.@type' : true,
-			'_META.@force' : [ false, 'boolean' ],
-			'@name' : isName,
-			'@mother' : [ false, isName ],
-			'@father' : [ false, isName ],
-			'@type' : false
-		},
-		defaults : {
-			'@type' : 'person'
-		},
-		extract : {
-			'Entity.0.@ID' : {
-				deps : [ '@name' ],
-				$ : findPersonByName
-			},
-			'Entity.1.@ID' : {
-				deps : [ '@mother' ],
-				$ : findPersonByName
-			},
-			'Entity.2.@ID' : {
-				deps : [ '@father' ],
-				$ : findPersonByName
-			},
-			'@motherIsFemale' : {
-				deps : [ 'Entity.1.@ID' ],
-				$ : function(ID) {
-					return _.findOne();
-				}
-			},
-			'@fatherIsMale' : {
-				deps : [ 'Entity.1.@ID' ],
-				$ : function(ID) {
-					return _.findOne();
-				}
-			}
-		},
-		transform : {
-			'@name' : 'Entity.0.Person.@name',
-			'@mother' : 'Entity.1.Person.@name',
-			'@father' : 'Entity.2.Person.@name',
-			'@type' : [ 'Entity.0.@type', 'Entity.1.@type', 'Entity.2.@type' ],
-			'Entity.2.@ID' : [ 'Entity.2.Belonging.@minorID', 'Entity.2.Belonging.@majorID' ]
-		},
-		load : {
-			Entity : {
-				filter : function(entity) {
-					return !entity['@ID'];
-				},
-				$ : function(entity) {
-					return _.create({
-						Entity : entity
-					});
-				},
-				Person : {
-					$ : function(person, entity) {
-						return _.create({
-							Person : _._.extend(person, {
-								'@ID': entity['@ID']
-							})
-						});
-					}
-				},
-				Belonging : {
-					$ : function(belonging, entity) {
-						return _.create({
-							Belonging : _._.extend(belonging, {
-								'@minorID': entity['@ID'],
-								'@majorID': entity['@ID']
-							})
-						});
-					}
-				}
-			}
-		}
-	};
+    return {
+        match: {
+            _: 'familyTree',
+            validate: {
+                '_META.@type': true,
+                '_META.@force': [false, 'boolean'],
+                '@name': isName,
+                '@mother': [false, isName],
+                '@father': [false, isName],
+                '@type': false
+            },
+            defaults: {
+                '@type': 'person'
+            }
+        },
+        extract: {
+            person: {
+                $: {name: '@name'},
+                view: 'person'
+            },
+            mother: {
+                $: {name: '@mother'},
+                view: 'person'
+            },
+            father: {
+                $: {name: '@father'},
+                view: 'person'
+            },
+            '@motherIsFemale': {
+                $: {entity: 'mother.Person.Entity.Entity'},
+                _: function ($) {
+                    return !$ || $.entity === null || $.entity.minorID !== $.entity.majorID;
+                }
+            },
+            '@fatherIsMale': {
+                $: {entity: 'father.Person.Entity.Entity'},
+                _: function ($) {
+                    return !$ || ($.entity !== null && $.entity.minorID === $.entity.majorID);
+                }
+            }
+        },
+        validate: {
+            '@motherIsFemale': true,
+            '@fatherIsMale': true
+        }
+    };
 };
--- src/test/examples/views/person.js
+++ src/test/examples/views/person.js
@@ -1,24 +1,33 @@
-module.exports = function(cpx, _, Q) {
-	return function($) {
-		return {
-			Person : {
-				_ : {
-					unique: true,
-					required: false,
-					where : {
-						name : $.name
-					}
-				},
-				Entity : {
-					_ : {
-						unique: true,
-						required: true,
-						where : {
-							type: 'person'
-						}
-					}
-				}
-			}
-		};
-	};
+module.exports = function (cpx, _, Q) {
+    return function ($) {
+        return {
+            Person: {
+                _: {
+                    unique: true,
+                    required: false,
+                    where: {
+                        name: $.name
+                    }
+                },
+                Entity: {
+                    _: {
+                        unique: true,
+                        required: true,
+                        where: {
+                            type: 'person'
+                        }
+                    },
+                    Entity: {
+                        _: {
+                            unique: true,
+                            required: false,
+                            where: {
+                                rank: 0
+                            }
+                        }
+                    }
+                }
+            }
+        };
+    };
 };
